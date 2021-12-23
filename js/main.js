928aa3103a6d3d48575fa40487a860653d661b48
Duy Anh <huynhanh.mtu@gmail.com>
2021-12-24 06:35:35 +0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,78 +1,57 @@
-import InfoData from "./InfoData.js";
-const infoData = InfoData;
+import Data from "./Data.js";
+const data = [...Data];
 
+// Render from data
+const renderData = () => {
+  const contentInner = data?.reduce((content, people, index) => {
+    const position = index === 0 ? "active" : index === (data.length - 1) ? "last" : "next";
+    return content += `
+      <article class="slider__item ${position}">
+        <img src="${people.img}" alt="${people.name} IMG">
+        <h4>${people.name}</h4>
+        <p class="slider__job">${people.job}</p>
+        <p class="slider__quote">${people.text}</p>
+        <div class="slider__icon">
+          <i class="fas fa-quote-right"></i>
+        </div>
+      </article>
+    `;
+  }, "");
 
+  document.querySelector(".slider__container").innerHTML = contentInner;
+}
+renderData();
 
-// const container = document.querySelector('.slide-container')
-// const nextBtn = document.querySelector('.next-btn')
-// const prevBtn = document.querySelector('.prev-btn')
-// // if length is 1 hide buttons
-// if (data.length === 1) {
-//   nextBtn.style.display = 'none'
-//   prevBtn.style.display = 'none'
-// }
-// // if length is 2, add copies of slides
-// let people = [...data]
-// if (data.length === 2) {
-//   people = [...data, ...data]
-// }
-// container.innerHTML = people
-//   .map((person, slideIndex) => {
-//     const { img, name, job, text } = person
-//     let position = 'next'
-//     if (slideIndex === 0) {
-//       position = 'active'
-//     }
-//     if (slideIndex === people.length - 1) {
-//       position = 'last'
-//     }
-//     if (data.length <= 1) {
-//       position = 'active'
-//     }
-//     return `<article class="slide ${position}">
-//   <img src=${img} class="img" alt="${name}"/>
-//   <h4>${name}</h4>
-//   <p class="title">${job}</p>
-//   <p class="text">
-//    ${text}
-//   </p>
-// <div class="quote-icon">
-// <i class="fas fa-quote-right"></i>
-// </div>
-//  </article>`
-//   })
-//   .join('')
+// Slideshow
+const startSlider = (type = undefined) => {
+  const active = document.querySelector(".active");
+  const last = document.querySelector(".last");
+  let next = active.nextElementSibling;
+  if (!next) {
+    next = document.querySelector(".slider__container").firstElementChild;
+  }
+  active.classList.remove("active");
+  last.classList.remove("last");
+  next.classList.remove("next");
 
-// const startSlider = (type) => {
-//   // get all three slides active,last next
-//   const active = document.querySelector('.active')
-//   const last = document.querySelector('.last')
-//   let next = active.nextElementSibling
-//   if (!next) {
-//     next = container.firstElementChild
-//   }
-//   active.classList.remove('active')
-//   last.classList.remove('last')
-//   next.classList.remove('next')
-
-//   if (type === 'prev') {
-//     active.classList.add('next')
-//     last.classList.add('active')
-//     next = last.previousElementSibling
-//     if (!next) {
-//       next = container.lastElementChild
-//     }
-//     next.classList.remove('next')
-//     next.classList.add('last')
-//     return
-//   }
-//   active.classList.add('last')
-//   last.classList.add('next')
-//   next.classList.add('active')
-// }
-// nextBtn.addEventListener('click', () => {
-//   startSlider()
-// })
-// prevBtn.addEventListener('click', () => {
-//   startSlider('prev')
-// })
+  if (type === "prev") {
+    active.classList.add("next");
+    last.classList.add("active");
+    next = last.previousElementSibling;
+    if (!next) {
+      next = document.querySelector(".slider__container").lastElementChild;
+    }
+    next.classList.remove("next");
+    next.classList.add("last");
+    return
+  }
+  active.classList.add('last');
+  last.classList.add('next');
+  next.classList.add('active');
+}
+document.querySelector(".next__btn").addEventListener("click", () => {
+  startSlider();
+})
+document.querySelector(".prev__btn").addEventListener("click", () => {
+  startSlider('prev');
+})
